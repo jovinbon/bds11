0c3b1d503e2e616ccceb57931565c5f760ee2f14
jovinbon <jovinbon@hotmail.com>
2022-09-06 20:49:37 -0300

    Cap 11: desafio

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,9 +1,51 @@
+import { AxiosRequestConfig } from 'axios';
+import { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import Select from 'react-select';
+import { toast } from 'react-toastify';
+import { Department } from 'types/department';
+import { Employee } from 'types/employee';
+import { requestBackend } from 'util/requests';
 import './styles.css';
+
 
 const Form = () => {
 
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+
+  const history = useHistory();
+
+  const { register, handleSubmit, formState: { errors }, control } = useForm<Employee>();
+
+
+  useEffect(() => {
+    requestBackend({ url: "/departments", withCredentials: true})
+      .then(response => {
+        setSelectDepartments(response.data);
+      })
+  }, []);
+
+  const onSubmit = () => {
+
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: "/employees",
+      withCredentials: true
+    };
+
+    requestBackend(config)
+      .then(() => {
+        toast.info('Empregado cadastrado com sucesso')
+        history.push("/admin/employees");
+      })
+      .catch(() => {
+        toast.error('Erro ao cadastrar o empregado')
+      });
+  };
+
   const handleCancel = () => {
-    // to do
+    history.push("/admin/employees");
   };
 
   return (
@@ -11,26 +53,65 @@ const Form = () => {
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} data-testid="form">
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input {...register('name', {
+                  required: 'Campo obrigatório'
+                })}
+                  type="text"
+                  className={`form-control base-input ${errors.name ? 'is-invalid' : ''}`}
+                  placeholder="Nome do empregado"
+                  name="name"
+                  data-testid="name"
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input {...register('email', {
+                  required: 'Campo obrigatório.',
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email inválido.'
+                  }
+                })}
+                  type="text"
+                  className={`form-control base-input ${errors.email ? 'is-invalid' : ''}`}
+                  placeholder="Email"
+                  name="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
+              </div>
+
+              <div className="margin-bottom-30">
+                <label htmlFor="department" className='d-none'>Departments</label>
+                <Controller
+                  name="department"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select {...field}
+                      options={selectDepartments}
+                      classNamePrefix="employee-crud-select"
+                      isMulti
+                      getOptionLabel={(department: Department) => department.name}
+                      getOptionValue={(department: Department) => String(department.id)}
+                      inputId="department"
+                    />
+                  )}
+                />
+                {errors.department && (
+                  <div className="invalid-feedback d-block">Campo obrigatório</div>
+                )
+
+                }
               </div>
 
             </div>
